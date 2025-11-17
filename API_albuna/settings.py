c60cf941ec97c8ab435b722277c9e1afc44601c6
Panji824 <142293564+Panji824@users.noreply.github.com>
2025-11-17 22:27:21 +0700

    update setting

--- API_albuna/settings.py
+++ API_albuna/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # PENTING: Tambahkan default untuk mencegah kegagalan saat build/collectstatic
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-dummy-key-for-build-only')
+SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
